95bd1f5d0c2f60ce8aeaf32b7e4ba5c4021645db
mfwass <michael@mfwass.com>
2018-01-08 17:48:12 -0500

    general: add copyright tag

--- bot/base/BotGlobals.py
+++ bot/base/BotGlobals.py
@@ -1,3 +1,14 @@
+# Filename: BotGlobals.py
+# Author: mfwass
+# Date: January 8th, 2017
+#
+# The Legend of Pirates Online Software
+# Copyright (c) The Legend of Pirates Online. All rights reserved.
+#
+# All use of this software is subject to the terms of the revised BSD
+# license.  You should have received a copy of this license along
+# with this source code in a file named "LICENSE."
+
 APP_DESCRIPTION = "Discord bot by TLOPO. <3  https://github.com/TheLegendofPiratesOnline/discord-bot"
 
 LOCAL_SETTINGS_FILENAME = 'local_settings.json'
---
+++ bot/language/BotLocalizerEnglish.py
@@ -0,0 +1,10 @@
+# Filename: BotLocalizerEnglish.py
+# Author: mfwass
+# Date: January 8th, 2017
+#
+# The Legend of Pirates Online Software
+# Copyright (c) The Legend of Pirates Online. All rights reserved.
+#
+# All use of this software is subject to the terms of the revised BSD
+# license.  You should have received a copy of this license along
+# with this source code in a file named "LICENSE."
